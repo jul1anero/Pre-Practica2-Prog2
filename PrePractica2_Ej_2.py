9d8c819c0075c997259849f175fb342f4e948d9c
titosalinas <salinas.ariel02@gmail.com>
2022-08-22 16:48:39 -0300

    ej2

--- PrePractica2_Ej_2.py
+++ PrePractica2_Ej_2.py
@@ -3,5 +3,19 @@
 #El programa debe de mostrar por pantalla la lista de numeros originales y la lista de numeros impares.
 
 #INICIO
+listaNumeros=[]
+listaImpares=[]
+numeroUsuario = int(input("Intoroduca un numero: "))
+listaNumeros.append(numeroUsuario)
+decision = input("Introcudir otro?: ")
+while decision == "s" or decision == "S":
+    numeroUsuario = int(input("Intoroduca otro numero: "))
+    listaNumeros.append(numeroUsuario)
+    decision = input("Introcudir otro?")
 
+for x in listaNumeros:
+    if x % 2 !=0:
+        listaImpares.append(x)
+
+print("los impares son", listaImpares)
 #FIN
