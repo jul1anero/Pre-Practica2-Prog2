f806b8726325a54f0a68afd599d46f6dfff7ab0f
jul1anero <julianvazquez12@gmail.com>
2022-08-22 16:20:01 -0300

    ej1

--- PrePractica2_Ej_1.py
+++ PrePractica2_Ej_1.py
@@ -3,6 +3,15 @@
 #Nota : Hacerlo con la función max(a,b) y luego con una comparación
 
 #INICIO
-
+print("ingrese un numero")
+a = input()
+print("ingrese otro numero")
+b = input()
+mayor= max(a,b)
+print("el mayor es",mayor)
+if a > b:
+    print("el mayor es el primero", a)
+else:
+    print("el mayor es el segundo",b)
 
 #FIN
